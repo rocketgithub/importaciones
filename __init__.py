fc62b5600784c0c05f257ee6dd25d1944cbf5446
Rodrigo Fernández <rodrigo@aquih.com>
2023-04-13 16:21:20 +0000

    Desactivar

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 # -*- encoding: utf-8 -*-
 
-from . import models
-from . import report
+#from . import models
+#from . import report
--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name' : 'Importaciones',
-    'version' : '1.0',
+    'version' : '2.0',
     'category': 'Custom',
     'description': """Modulo de importaciones de Prisma.""",
     'author': 'Rodrigo Fernández',
@@ -10,12 +10,6 @@
     'depends' : ['purchase_stock'],
     'demo' : [ ],
     'data' : [
-        'views/polizas_view.xml',
-        'views/report.xml',
-        'views/poliza.xml',
-        'views/purchase_view.xml',
-        'security/ir.model.access.csv',
-        'security/importaciones_security.xml',
     ],
     'installable': True,
 }
